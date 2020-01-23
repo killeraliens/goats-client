1ebbdf0c99b432760e354652d37168bf31a23281
killeraliens <killeraliens@outlook.com>
2020-01-23 15:30:10 -0700

    dummydata for static

---
+++ src/DUMMY.js
@@ -0,0 +1,271 @@
+export default {
+  "users": [
+    {
+      "id": "3",
+      "username": "killeraliens",
+      "password_digest": "hello123",
+      "token": "sampletoken",
+      "admin": "false",
+      "email": "killeraliens@outlook.com",
+      "image_url": "",
+      "created": "2019-01-01T00:00:00.000Z",
+      "last_login": "2020-01-23T20:47:47.355Z",
+      "city_name": "",
+      "region_name": "",
+      "country_name": ""
+    },
+    {
+      "id": "666",
+      "username": "devilmaster",
+      "password_digest": "hello123",
+      "token": "sampletoken",
+      "admin": "false",
+      "email": "devilmaster@sampleemail.com",
+      "image_url": "https://media2.fdncms.com/sacurrent/imager/u/original/17106401/devils_3_legend_production_company.jpg",
+      "created": "2019-01-03T00:00:00.000Z",
+      "last_login": "2019-01-03T18:00:00.000Z",
+      "city_name": "Copenhagen",
+      "region_name": "",
+      "country_name": "Denmark"
+    },
+    {
+      "id": "667",
+      "username": "greyalien",
+      "password_digest": "hello123",
+      "token": "sampletoken",
+      "admin": "false",
+      "email": "greyalien@sampleemail.com",
+      "image_url": "https://www.newshub.co.nz/dam/form-uploaded-images/roswell-ufo-alien-CREDIT-GETTY-1120.jpg",
+      "created": "2019-01-05T19:22:00.000Z",
+      "last_login": "2019-01-05T20:42:00.000Z",
+      "city_name": "Ghent",
+      "region_name": "LUX",
+      "country_name": "Belgium"
+    }
+  ],
+
+  "flyers": [
+    {
+      "id": "1",
+      "creator_id": "666",
+      "type": "Festival",
+      "image_url": "https://lastfm.freetls.fastly.net/i/u/ar0/02164795f45ff2dfd34bbc06c584bcb2.webp",
+      "headline": "Total Death Over Mexico lll",
+      "created": "2019-01-03T16:00:00.000Z",
+      "modified": "2019-01-03T16:00:00.000Z",
+      "bands": "",
+      "details": "Foro San Rafael Av. Ribera de San Cosme #28, 06470 Mexico City, Mexico https://www.facebook.com/events/foro-san-rafael/total-death-over-mexico-lll-informativo/1004824666540845/",
+      "publish_comment": "could someone comment with an official list of bands?",
+      "listing_state": "Public"
+    },
+    {
+      "id": "2",
+      "creator_id": "667",
+      "type": "Tour",
+      "image_url": "http://www.brooklynvegan.com/files/2019/11/75521841_2571097956311369_9029394387301302272_o1.jpg?w=630",
+      "headline": "RIPPIKOULU / CHTHE’ILIST / NUCLEUS — 2020 North American Tour",
+      "created": "2019-01-05T19:42:00.000Z",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "bands": "RIPPIKOULU, CHTHE’ILIST, NUCLEUS",
+      "details": "Read More: Finnish death doom vets Rippikoulu touring with Chthe’ilist & Nucleus | http://www.brooklynvegan.com/finnish-death-doom-vets-rippikoulu-touring-with-chtheilist-nucleus/?trackback=tsmclip",
+      "publish_comment": "Found this on [Brooklyn Vegan](http://www.brooklynvegan.com/finnish-death-doom-vets-rippikoulu-touring-with-chtheilist-nucleus/)",
+      "listing_state": "Public"
+    },
+    {
+      "id": "3",
+      "creator_id": "3",
+      "type": "Festival",
+      "image_url": "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/81886107_2710011682400996_3673221855931531264_o.jpg?_nc_cat=109&_nc_ohc=7ZAgDasafVUAX9nfoil&_nc_ht=scontent-lax3-1.xx&oh=6d8ac1b747afa31ae57800526791a2ae&oe=5ED59271",
+      "headline": "Killtown Deathfest VIII - '10 Years of Death'",
+      "created": "2020-01-23T21:42:14.355Z",
+      "modified": "2020-01-23T21:42:14.355Z",
+      "bands": "Lineup TBA",
+      "details": "Pumpehuset Studiestræde 52, 1554 Copenhagen https://www.facebook.com/events/2806422816089129/",
+      "publish_comment": "",
+      "listing_state": "Public"
+    }
+  ],
+
+  "events": [
+    {
+      "id": "1",
+      "flyer_id": "1",
+      "date": "2020-04-12",
+      "venue_name": "Foro San Rafael",
+      "modified": "2019-01-03T16:00:00.000Z",
+      "city_name": "Mexico City",
+      "region_name": "",
+      "country_name": "Mexico"
+    },
+    {
+      "id": "2",
+      "flyer_id": "1",
+      "date": "2020-04-13",
+      "venue_name": "Foro San Rafael",
+      "modified": "2019-01-03T16:00:00.000Z",
+      "city_name": "Mexico City",
+      "region_name": "",
+      "country_name": "Mexico"
+    },
+    {
+      "id": "3",
+      "flyer_id": "1",
+      "date": "2020-04-14",
+      "venue_name": "Foro San Rafael",
+      "modified": "2019-01-03T16:00:00.000Z",
+      "city_name": "Mexico City",
+      "region_name": "",
+      "country_name": "Mexico"
+    },
+    {
+      "id": "4",
+      "flyer_id": "1",
+      "date": "2020-04-15",
+      "venue_name": "Foro San Rafael",
+      "modified": "2019-01-03T16:00:00.000Z",
+      "city_name": "Mexico City",
+      "region_name": "",
+      "country_name": "Mexico"
+    },
+    {
+      "id": "5",
+      "flyer_id": "2",
+      "date": "2020-04-03",
+      "venue_name": "Maple Grove Tavern",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Clevland",
+      "region_name": "OH",
+      "country_name": "United States"
+    },
+    {
+      "id": "6",
+      "flyer_id": "2",
+      "date": "2020-04-04",
+      "venue_name": "TBD",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Detroit",
+      "region_name": "MI",
+      "country_name": "United States"
+    },
+    {
+      "id": "7",
+      "flyer_id": "2",
+      "date": "2020-04-05",
+      "venue_name": "Cobra Lounge",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Chicago",
+      "region_name": "IL",
+      "country_name": "United States"
+    },
+    {
+      "id": "8",
+      "flyer_id": "2",
+      "date": "2020-04-06",
+      "venue_name": "Black Circle Brewing",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Indianapolis",
+      "region_name": "IN",
+      "country_name": "United States"
+    },
+    {
+      "id": "9",
+      "flyer_id": "2",
+      "date": "2020-04-07",
+      "venue_name": "TBD",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Columbus",
+      "region_name": "OH",
+      "country_name": "United States"
+    },
+    {
+      "id": "10",
+      "flyer_id": "2",
+      "date": "2020-04-08",
+      "venue_name": "Cativo's",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Pittsburgh",
+      "region_name": "PA",
+      "country_name": "United States"
+    },
+    {
+      "id": "11",
+      "flyer_id": "2",
+      "date": "2020-04-09",
+      "venue_name": "Metro Gallery",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Baltimore",
+      "region_name": "MD",
+      "country_name": "United States"
+    },
+    {
+      "id": "12",
+      "flyer_id": "2",
+      "date": "2020-04-10",
+      "venue_name": "Kung Fu Necktie",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Philadelphia",
+      "region_name": "PA",
+      "country_name": "United States"
+    },
+    {
+      "id": "13",
+      "flyer_id": "2",
+      "date": "2020-04-11",
+      "venue_name": "Saint Vitus",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "New York",
+      "region_name": "NY",
+      "country_name": "United States"
+    },
+    {
+      "id": "14",
+      "flyer_id": "2",
+      "date": "2020-04-12",
+      "venue_name": "Piranha Bar",
+      "modified": "2019-01-05T19:42:00.000Z",
+      "city_name": "Montreal",
+      "region_name": "QC",
+      "country_name": "Canada"
+    },
+    {
+      "id": "15",
+      "flyer_id": "3",
+      "date": "2020-09-03",
+      "venue_name": "Pumpehuset",
+      "modified": "2020-01-23T21:42:14.355Z",
+      "city_name": "Copenhagen",
+      "region_name": "",
+      "country_name": "Denmark"
+    },
+    {
+      "id": "16",
+      "flyer_id": "3",
+      "date": "2020-09-04",
+      "venue_name": "Pumpehuset",
+      "modified": "2020-01-23T21:42:14.355Z",
+      "city_name": "Copenhagen",
+      "region_name": "",
+      "country_name": "Denmark"
+    },
+    {
+      "id": "17",
+      "flyer_id": "3",
+      "date": "2020-09-05",
+      "venue_name": "Pumpehuset",
+      "modified": "2020-01-23T21:42:14.355Z",
+      "city_name": "Copenhagen",
+      "region_name": "",
+      "country_name": "Denmark"
+    },
+    {
+      "id": "18",
+      "flyer_id": "3",
+      "date": "2020-09-06",
+      "venue_name": "Pumpehuset",
+      "modified": "2020-01-23T21:42:14.355Z",
+      "city_name": "Copenhagen",
+      "region_name": "",
+      "country_name": "Denmark"
+    },
+  ]
+}
