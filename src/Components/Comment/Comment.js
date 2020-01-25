3a2f38a362d0bd5659c1059b41fe5043dec81254
killeraliens <killeraliens@outlook.com>
2020-01-25 15:57:46 -0700

    publish comment only

--- src/Components/Comment/Comment.js
+++ src/Components/Comment/Comment.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Avatar from '../Avatar/Avatar';
-export default function Comment({ user, isCreator, comment, modified }) {
+import './Comment.css';
+import FlyerCard from '../FlyerCard/FlyerCard';
 
+export default function Comment({ user, isCreator, comment, modified }) {
+  const modifiedAt = new Date(modified).toLocaleString()
   return (
     <div className="Comment">
         <div className="Comment--header">
@@ -10,16 +14,30 @@ export default function Comment({ user, isCreator, comment, modified }) {
               imageUrl={user.image_url}
               username={user.username}
             />
-            <h3 className="Comment--handle">
-              @killeraliens
-                  <span className="Comment--isCreator">[creator]</span>
+            <h3 className="Comment--handle username">
+              {user.username}
+              {isCreator ? <span className="Comment--isCreator">[creator]</span> : null}
             </h3>
           </div>
-          <span className="Comment--modified-at">mentioned: Jan 10, 2020 - 22:33 MST</span>
+          <span className="Comment--modified-at">
+             {/* Jan 10, 2020 - 22:33 MST */}
+          mentioned: {modifiedAt}
+            </span>
         </div>
-        <p>could someone comment with an official list of bands?</p>
+        {comment ? <p className="Comment--comment">{comment}</p> : null}
     </div>
 
   )
 }
 
+Comment.defaultProps = {
+  isCreator: false
+}
+
+Comment.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string
+  }),
+  isCreator: PropTypes.bool
+}
