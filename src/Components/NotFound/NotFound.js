d8874fec48879aeebab56658dad2ad5cb981ae10
killeraliens <killeraliens@outlook.com>
2020-02-13 11:21:58 -0700

    remove dummy escape

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -18,7 +18,6 @@ export default function NotFound({ message, link }) {
           <div>
             <p>{message}</p>
             <p><Link to={`/dashboard/${context.user.id}`}>Back to dashboard</Link></p>
-            <p> <Link to="/public/signin">Back to sign in (temp dummy escape hatch)</Link></p>
           </div >
         )
         : Boolean(context.user) && Boolean(context.user.id) && !!link
@@ -26,7 +25,6 @@ export default function NotFound({ message, link }) {
           <div>
             <p>{message}</p>
             <p> {link}</p>
-            <p> <Link to="/public/signin">Back to sign in (temp dummy escape hatch)</Link></p>
           </div >
         )
         : (
