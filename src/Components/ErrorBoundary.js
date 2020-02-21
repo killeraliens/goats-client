7d541fbca3177b01614cf9a5810a2356262c705d
killeraliens <killeraliens@outlook.com>
2020-02-21 09:52:12 -0700

    master error set test, not found error message

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -36,10 +36,13 @@ class ErrorBoundary extends Component {
 
   render() {
     if (this.state.hasError) {
+      console.log('error bound triggered', this.state.error)
       return (
         <div className='ErrorBoundary'>
           <NotFound
-            message={this.state.error.status === 401 ? 'Session Expired' : 'Something went wrong'}
+            message={this.state.error.status === 401
+              ? 'Session Expired'
+              : 'Something went wrong'}
             link={<Link to="/public/signin">Sign In</Link>}/>
         </div>
       )
