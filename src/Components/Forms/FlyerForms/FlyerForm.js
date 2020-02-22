5ee2d562470f359d9e8e2eca6de8eb2627ba1f20
killeraliens <killeraliens@outlook.com>
2020-02-22 15:42:54 -0700

    removed client side event id

--- src/Components/Forms/FlyerForms/FlyerForm.js
+++ src/Components/Forms/FlyerForms/FlyerForm.js
@@ -194,7 +194,6 @@ export default function FlyerForm({ history, newType, flyer, events, creatorId }
         } else if ((formBody.date.touched && !Boolean(formBody.date.error)) && (formBody.endDate.touched && !Boolean(formBody.endDate.error))) {
           for (let i = 0; i <= dayCount && i < (dayCount +1); i++) {
             let newEvent = {
-              id: uuid(),
               date: addDaysToDateReturnMMDDString(formBody.date.value, i),
               venueName: formBody.venueName.value,
               countryName: formBody.countryName.value,
