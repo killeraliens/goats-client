33a6fd0973e1cd89e60cb41446910b67654c06b0
killeraliens <killeraliens@outlook.com>
2020-03-08 20:50:08 -0700

    added fixed back to app, no disabled body scroll

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import AuthedSplit from './Components/AuthedSplit/AuthedSplit';
 import CreateFlyer from './Components/CreateFlyer/CreateFlyer';
 import GetFlyer from './Components/GetFlyer/GetFlyer'
 import NotFound from './Components/NotFound/NotFound';
-import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+// import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 
 class App extends Component {
   constructor() {
@@ -24,14 +24,14 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    const appElement = document.getElementById('App')
-    enableBodyScroll(appElement);
-  }
+  // componentDidMount() {
+  //   const appElement = document.getElementById('App')
+  //   enableBodyScroll(appElement);
+  // }
 
-  componentWillUnmount() {
-    clearAllBodyScrollLocks()
-  }
+  // componentWillUnmount() {
+  //   clearAllBodyScrollLocks()
+  // }
 
   updateAuthenticated = (user) => {
     this.setState({
