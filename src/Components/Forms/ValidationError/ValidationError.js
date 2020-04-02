8c07e68b824b475eccb4076c4032649cd2446ea9
killeraliens <killeraliens@outlook.com>
2020-04-02 11:30:19 -0700

    style prop added for now, make it a server-err class later

--- src/Components/Forms/ValidationError/ValidationError.js
+++ src/Components/Forms/ValidationError/ValidationError.js
@@ -2,19 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './ValidationError.css'
 
-export default function ValidationError({ id, message }) {
+export default function ValidationError({ id, message, style }) {
 
   return message
-    ? <span id={id} className={`ValidationError`}>{message}</span>
-    : <span id={id} className={`ValidationError sr-only`}>No error</span>
+    ? <span id={id} className={`ValidationError`} style={style}>{message}</span>
+    : <span id={id} className={`ValidationError sr-only`} style={style}>No error</span>
 }
 
 ValidationError.propTypes = {
   message: PropTypes.string,
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  style: PropTypes.object
 }
 
 ValidationError.defaultProps = {
-  message: ''
+  message: '',
+  style: {}
 }
 
