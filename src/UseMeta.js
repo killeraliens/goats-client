568e7f08743f95dfcd4ae8cd2d829944491f8052
killeraliens <killeraliens@outlook.com>
2020-04-21 16:02:33 -0700

    meta og url from cloud

--- src/UseMeta.js
+++ src/UseMeta.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
 import MetaTags from 'react-meta-tags'
 import metaImage from './assets/goats-arm-meta.png'
-import API_ENDPOINT  from './config'
 
 class UseMeta extends Component {
   render() {
     return (
      <MetaTags>
-        <title>GOATS GUIDE PG</title>
-        <meta name="description" content="Some description." />
+        <title>GOATS GUIDE</title>
+        <meta
+          name="description"
+          content="Know of an upcoming concert, fest, or tour? Share the flyer here. Digitally archive posters and event artwork."
+        />
+        <meta
+          name="keywords"
+          content="goats guide, goatsguide, fliers, flyers, poster archive, metal, concerts, festivals, metal shows, metal events, flyer archive"
+        />
         <meta property="og:title" content="GOATS GUIDE" />
-        <meta property="og:image" content={`${API_ENDPOiNT}/goats-arm-meta.png`} />
+        <meta property="og:description" content="Know of an upcoming concert, fest, or tour? Share the flier here. Digitally archive posters and event artwork." />
+        <meta property="og:url" content="goatsguide.com" />
+        <meta property="og:image" content="https://res.cloudinary.com/killeraliens/image/upload/v1587509079/goats-arm-meta.png" />
       </MetaTags>
     )
   }
