8910b37794fc314b49526d7d035d4d1832d2a782
killeraliens <killeraliens@outlook.com>
2020-04-21 13:53:38 -0700

    html links broken

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import UseMeta from './UseMeta'
+// import UseMeta from './UseMeta'
 
 ReactDOM.render(
 <BrowserRouter>
