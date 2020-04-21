4f3d07cc6bc26a37cec69d55385e45c93fda7e8f
killeraliens <killeraliens@outlook.com>
2020-04-21 14:52:04 -0700

    api endpoint attempt

--- src/UseMeta.js
+++ src/UseMeta.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import MetaTags from 'react-meta-tags'
 import metaImage from './assets/goats-arm-meta.png'
+import API_ENDPOINT  from './config'
 
 class UseMeta extends Component {
   render() {
@@ -10,7 +11,7 @@ class UseMeta extends Component {
         <title>GOATS GUIDE PG</title>
         <meta name="description" content="Some description." />
         <meta property="og:title" content="GOATS GUIDE" />
-        <meta property="og:image" content={metaImage} />
+        <meta property="og:image" content={`${API_ENDPOiNT}/goats-arm-meta.png`} />
       </MetaTags>
     )
   }
