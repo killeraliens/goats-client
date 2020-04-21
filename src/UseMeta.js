df8c7504304b204ed8032af99a6050e5525b562e
killeraliens <killeraliens@outlook.com>
2020-04-21 13:49:33 -0700

    rm interpolation index

--- src/UseMeta.js
+++ src/UseMeta.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom'
 import MetaTags from 'react-meta-tags'
 import metaImage from './assets/goats-arm-meta.png'
 
-export default function UseMeta () {
-  return(
-    <MetaTags>
-      <title>GOATS GUIDE PG</title>
-      <meta name="description" content="Some description." />
-      <meta property="og:title" content="GOATS GUIDE" />
-      <meta property="og:image" content={metaImage} />
-    </MetaTags>
-  )
+class UseMeta extends Component {
+  render() {
+    return (
+     <MetaTags>
+        <title>GOATS GUIDE PG</title>
+        <meta name="description" content="Some description." />
+        <meta property="og:title" content="GOATS GUIDE" />
+        <meta property="og:image" content={metaImage} />
+      </MetaTags>
+    )
+  }
 }
+export default UseMeta
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import UseMeta from './UseMeta'
 
 ReactDOM.render(
 <BrowserRouter>
+  <UseMeta />
   <App />
 </BrowserRouter>, document.getElementById('root'))
 
