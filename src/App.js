a107e0b38d3045eae69dbf5fbbcda04cd96fff72
David Alexander Vega <davidalexandervega@gmail.com>
2022-06-02 13:13:50 -0700

    refactored from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
 import './App.css'
 import AppContext from './AppContext'
 import PrivateRoute from './Components/PrivateRoute'
@@ -12,106 +12,98 @@ import CreateFlyer from './Components/CreateFlyer/CreateFlyer'
 import GetFlyer from './Components/GetFlyer/GetFlyer'
 import KillerToast from './Components/KillerToast/KillerToast'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: JSON.parse(localStorage.getItem('user')) || null,
-      error: null,
-      fetching: false,
-      toast: { on: false, message: '', colorClass: '' }
-    }
+const App = () => {
+
+  const [auth, setAuth] = useState({
+    user: JSON.parse(localStorage.getItem('user')) || null,
+    error: null,
+    fetching: false,
+    toast: { on: false, message: '', colorClass: '' }
+  })
+
+  const updateAuthenticated = (user) => {
+    setAuth((prevState) => ({
+      ...prevState,
+      user: user
+    }))
+    localStorage.setItem("user", JSON.stringify(auth.user))
   }
 
-  updateAuthenticated = (user) => {
-    this.setState({
-      user
-    }, () => {
-      localStorage.setItem("user", JSON.stringify(this.state.user))
-    })
+  const updateUser = (newProps) => {
+    setAuth((prevState) => ({
+      ...prevState,
+      user: {...auth.user, ...newProps}
+    }))
+    localStorage.setItem("user", JSON.stringify(auth.user))
   }
 
-  updateUser = (newProps) => {
-    this.setState({
-      user: {...this.state.user, ...newProps}
-    }, () => {
-      localStorage.setItem("user", JSON.stringify(this.state.user))
-    })
+  const setError = (message) => {
+    setAuth((prevState) => ({...prevState, error: message }))
   }
 
-  destroyCurrentLoginState = () => {
-    this.updateAuthenticated(null)
-  }
-
-  setError = (message) => {
-    this.setState({ error: message })
-  }
-
-  setToast = ({ message, colorClass, timeout: timeout=1500 }) => {
+  const setToast = ({ message, colorClass, timeout: timeout=1500 }) => {
     setTimeout(() => {
-      this.setState({ toast: { on: true, message: message, colorClass: colorClass }})
+      setAuth((prevState) => ({...prevState, toast: { on: true, message: message, colorClass: colorClass }}))
     }, 200);
     setTimeout(() => {
-      this.setState({ toast: { on: false, message: '', colorClass: '' }})
+      setAuth((prevState) => ({...prevState, toast: { on: false, message: '', colorClass: '' }}))
     }, timeout);
   }
 
-  render() {
-    const context = {
-      user: this.state.user,
-      updateAuthenticated: this.updateAuthenticated,
-      updateUser: this.updateUser,
-      error: this.state.error,
-      setError: this.setError,
-      toast: this.state.toast,
-      setToast: this.setToast,
-      fetching: this.state.fetching
-    }
+  const history = useHistory();
+  useEffect(() => {
+    !user ? history.push("/public/signin") : history.push("/flyers")
+  }, [auth.user])
 
-    const { toast, user } = this.state
-    return(
-      <div className="App" id="App" >
-        <AppContext.Provider value={context}>
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route path="/public/:action" component={Landing}/>
-              <PrivateRoute path={`/dashboard/:user_id`} render={props =>
-                <ErrorBoundary>
-                  <AuthedSplit mainComponent={<Dashboard {...props}/>} />
-                </ErrorBoundary>
-              } />
-              <PrivateRoute path={`/flyers`} render={props =>
-                <ErrorBoundary>
-                  <AuthedSplit mainComponent={<Forum {...props}/>} />
-                </ErrorBoundary>
-              } />
-              <PrivateRoute path={`/post`} render={props =>
-                <ErrorBoundary>
-                  <AuthedSplit mainComponent={<CreateFlyer {...props} />} />
-                </ErrorBoundary>
-              } />
-              <PrivateRoute path={`/flyer/:flyer_id/edit`} render={props =>
-                <ErrorBoundary>
-                  <AuthedSplit mainComponent={<GetFlyer isEdit={true} {...props} />} />
-                </ErrorBoundary>
-              } />
-              <PrivateRoute path={`/flyer/:flyer_id`} render={props =>
-                <ErrorBoundary>
-                  <AuthedSplit mainComponent={<GetFlyer {...props} />} />
-                </ErrorBoundary>
-              } />
-            </Switch>
-            <Route render={() => {
-              return !user
-                    ? <Redirect to="/public/signin" /> // <NotFound link={<Link to="/public/signin">Sign In</Link>} />
-                    : <Redirect to="/flyers" /> // <NotFound link={<Link to="/flyers">Back to forum</Link>} />
-                }
-              } />
-            <KillerToast on={toast.on} message={toast.message} colorClass={toast.colorClass}/>
-        </ AppContext.Provider >
-      </div>
-    )
+  const context = {
+    user: auth.user,
+    updateAuthenticated: updateAuthenticated,
+    updateUser: updateUser,
+    error: auth.error,
+    setError: setError,
+    toast: auth.toast,
+    setToast: setToast,
+    fetching: auth.fetching
   }
-}
+
+  const { toast, user } = auth
+
+  return (
+    <div className="App" id="App" >
+      <AppContext.Provider value={context}>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/public/:action" component={Landing}/>
+          <PrivateRoute path={`/dashboard/:user_id`} render={props =>
+            <ErrorBoundary>
+              <AuthedSplit mainComponent={<Dashboard {...props}/>} />
+            </ErrorBoundary>
+          } />
+          <PrivateRoute path={`/flyers`} render={props =>
+            <ErrorBoundary>
+              <AuthedSplit mainComponent={<Forum {...props}/>} />
+            </ErrorBoundary>
+          } />
+          <PrivateRoute path={`/post`} render={props =>
+            <ErrorBoundary>
+              <AuthedSplit mainComponent={<CreateFlyer {...props} />} />
+            </ErrorBoundary>
+          } />
+          <PrivateRoute path={`/flyer/:flyer_id/edit`} render={props =>
+            <ErrorBoundary>
+              <AuthedSplit mainComponent={<GetFlyer isEdit={true} {...props} />} />
+            </ErrorBoundary>
+          } />
+          <PrivateRoute path={`/flyer/:flyer_id`} render={props =>
+            <ErrorBoundary>
+              <AuthedSplit mainComponent={<GetFlyer {...props} />} />
+            </ErrorBoundary>
+          } />
+        </Switch>
+            <KillerToast on={toast.on} message={toast.message} colorClass={toast.colorClass}/>
+      </AppContext.Provider>
+    </div>
+  );
+};
 
 export default App;
